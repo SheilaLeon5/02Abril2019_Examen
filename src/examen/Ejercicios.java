30d8ad06f1a92cef25f786bd59502e9837ff5272
Sheila <Sheila@DESKTOP-1PIPS59>
2019-04-02 09:14:26 +0100

    Act 1.-

--- src/examen/Ejercicios.java
+++ src/examen/Ejercicios.java
@@ -22,47 +22,51 @@ public class Ejercicios {
 		String registroJugadores = null;
 		String idEquipo;
 		String nombreLargo;
-		String palabraFinalizar = ".";
+		ArrayList<Jugador> listaJugadores = new ArrayList<Jugador>();
 
 		try {
-			BufferedReader fichero = new BufferedReader(new FileReader("ficheros/equipos.txt")); //Leer fichero (comparar lo escrito por teclado)
+			BufferedReader fichero = new BufferedReader(new FileReader("ficheros/equipos.txt"));
 			BufferedReader ficheroJug = new BufferedReader(new FileReader(rutaJugadores));
 
-				while((registro=fichero.readLine()) !=null) {
-					String[] campos = registro.split("#");
-					idEquipo = campos[0];
-					nombreLargo = campos[2];
-					if(tecleado.equals(nombreLargo)) {
-						System.out.println(nombreLargo + " ha sido escrito");
+			while((registro=fichero.readLine()) !=null) {
+				String[] campos = registro.split("#");
+				idEquipo = campos[0];
+				nombreLargo = campos[2];
 
-						//Leer jugadores
-						while((registroJugadores= ficheroJug.readLine())!= null) {
-							String[]camposJug = registroJugadores.split("#");
-							if (idEquipo.equals(camposJug[4])) {
-								System.out.println(camposJug[2]);
-							}
+				if (equipo.compareToIgnoreCase(nombreLargo)==0) {
+					//Leer jugadores
+					while((registroJugadores= ficheroJug.readLine())!= null) {
+						String[]camposJug = registroJugadores.split("#");
+						if (idEquipo.equals(camposJug[7])) {
+							//Jugador jugador = new Jugador(camposJug[1], camposJug[2], Integer.parseInt(camposJug[2]), camposJug[3], camposJug[4].charAt(0), Integer.parseInt(camposJug[5]), Integer.parseInt(camposJug[6]), Integer.parseInt(camposJug[7]));							
+							
+							Jugador jugador = new Jugador();
+							jugador.setId(Integer.parseInt(campos[0]));
+							jugador.setNif(campos[1]);
+							jugador.setNombre(campos[2]);
+							jugador.set
+							listaJugadores.add(jugador);
 						}
-					}
+					}	
 				}
-
-			teclado.close(); fichero.close();
+			}
+			fichero.close();
+			return listaJugadores;
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}	catch (IOException e) {
 			System.out.println(e.getMessage());
-		}	
+		}
+		return null;	
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return null;
-		
+	}
+	
+	
+	
+	public static void main(String[]args){
+		// 
+		 Ejercicios ejercicios= new Ejercicios();
+		 ArrayList<Jugador> resultadoEjercicio1 = ejercicios.ejercicio1("ficheros/jugadores.txt", "REAL MADRID");
+		 System.out.println("h");
 	}
 }
